a50af420e1436217cfda332f2a34ed8362f792ec
tod01 <git@bitbucket.org:tod01>
2019-11-22 17:57:28 +0100

    GUI implement

---
+++ src/GUI/MidiGui.java
@@ -0,0 +1,76 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+
+public class MidiGui extends JFrame {
+
+    private static final String TITLE = "Midi displayed";
+    private int with;
+    private int height;
+    String file;
+    static JFrame jFrame;
+    static JLabel jLabel;
+
+
+    public MidiGui(int with, int height, String file) {
+        this.with = with;
+        this.height = height;
+        this.file = file;
+    }
+
+    public void launcher() throws IOException, InterruptedException, ParseException {
+        jFrame = new JFrame("Karaoke");
+        jLabel = new JLabel();
+        jLabel.setVerticalAlignment(JLabel.CENTER);
+        jLabel.setHorizontalAlignment(JLabel.CENTER);
+        jLabel.setText("Music");
+        JPanel jPanel = new JPanel();
+        jPanel.add(jLabel);
+        jFrame.add(jPanel);
+        jFrame.setSize(with, height);
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+
+        BufferedReader bufferedReader = new BufferedReader((new InputStreamReader(new FileInputStream(file))));
+        String line;
+        DateFormat dateFormat = new SimpleDateFormat("mm:ss.SS");
+        long reference = 0;
+        Calendar calendar = Calendar.getInstance();
+        while((line = bufferedReader.readLine()) != null) {
+
+            if(line.startsWith("[")) {
+                String timeText = line.split("]")[0].substring(1);
+                Date date = dateFormat.parse(timeText);
+
+                calendar.setTime(date);
+
+                long milliseconds = 60000 * calendar.get(Calendar.MINUTE) + 1000* calendar.get(Calendar.SECOND) + calendar.get(Calendar.MILLISECOND);
+                long result = milliseconds - reference;
+                reference = milliseconds;
+                Thread.sleep(result);
+                System.out.println(line + " | " + " " + calendar.get(Calendar.MINUTE) + ":"+ calendar.get(Calendar.SECOND) + ":" + calendar.get(Calendar.MILLISECOND) + " " + milliseconds + " " + result);
+                //label.setText(line);
+                //add(label);
+                jLabel.setText(line);
+                // jLabel.setVerticalAlignment(SwingConstants.CENTER);
+                // jLabel.setHorizontalAlignment(SwingConstants.CENTER);
+                // jLabel.setText(line);
+            }
+        }
+
+        bufferedReader.close();
+
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException, ParseException {
+        new MidiGui(500, 500, "MidiFiles/The-Black-Eyed-Peas-I-Gotta-Feeling.lrc").launcher();
+    }
+}
